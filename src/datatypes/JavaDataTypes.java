0b6064dcdbb6d28b76fd5a99f238b2da663a1eb2
Giorgi <giorgi.webdev@gmail.com>
2022-03-08 16:30:26 -0500

    Assignment Data Types

--- src/datatypes/JavaDataTypes.java
+++ src/datatypes/JavaDataTypes.java
@@ -3,6 +3,20 @@ package datatypes;
 public class JavaDataTypes {
     public static void main(String[] args) {
 
+        /**
+         * Due: Mar 6 (Sunday)
+         *
+         * Store values for below scenario in an appropriate variable, and print the same in Console.
+         *  1. number of hours in a day
+         *  2. max no. of days in an year
+         *  3. total number of employees in an organization
+         *  4. Population in a country
+         *  5. Mortgage rate
+         *  6. Balance in an bank account
+         *  7. does the sun rise from the West?
+         *  8. Initials of your name (Use: char)    // My initials are DS
+         *  9. Store your full name (Use: String)
+         */
 
         //1
         byte hoursOfDay = 24;
@@ -40,21 +54,5 @@ public class JavaDataTypes {
         //9
         String myName = "Giorgi Tsereteli";
         System.out.println("Full Name - "+myName);
-
-
-        /**
-         * Due: Mar 6 (Sunday)
-         *
-         * Store values for below scenario in an appropriate variable, and print the same in Console.
-         *  1. number of hours in a day
-         *  2. max no. of days in an year
-         *  3. total number of employees in an organization
-         *  4. Population in a country
-         *  5. Mortgage rate
-         *  6. Balance in an bank account
-         *  7. does the sun rise from the West?
-         *  8. Initials of your name (Use: char)    // My initials are DS
-         *  9. Store your full name (Use: String)
-         */
     }
 }
